a0c6d899ca360883192617d98b346751347f12e2
TinaKovalova <kovalevatkv@gmail.com>
2024-09-26 17:45:03 +0300

    burger menu

---
+++ js/index.js
@@ -0,0 +1,40 @@
+
+
+// const details = document.querySelectorAll(".menu__details-block");
+// console.log(details)
+// if (details.length > 0) {
+//     for (let i = 0; i < details.length; i++){
+//         console.log(details[i]);
+//         const sammary = details[i].querySelector("summary");
+//         sammary.addEventListener("click", (e) => {
+//                    e.preventDefault();
+//             details[i].classList.toggle('_open');
+//             console.log(
+//                 details[i].shadowRoot
+//             );
+//             details[i].shadowRoot.querySelector('#details-content')
+//             //   "content-visibility: hidden; display: block;";
+
+          
+               
+
+//           });
+
+//     }
+// }
+
+const burgerBtn = document.querySelector(".burger-btn");
+const burgerMenu = document.querySelector(".burger-menu");
+
+burgerBtn.addEventListener("click", (e) => {
+  e.currentTarget.classList.toggle("burger-btn--active");
+  burgerMenu.classList.toggle("burger-menu--active");
+});
+burgerMenu.addEventListener("click", (e) => {
+  if (e.target.closest("[class$='__link']")) {
+    burgerBtn.classList.toggle("burger-btn--active");
+    burgerMenu.classList.toggle("burger-menu--active");
+  }
+});
+
+
